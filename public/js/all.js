f8a94883627f69b117e018f006fadae507a20af3
mauran <maurakiri@gmail.com>
2018-02-26 15:58:33 +0530

    changes

--- public/js/all.js
+++ public/js/all.js
@@ -12,11 +12,24 @@ function allblocks(){
     window.location.href = '/allblocks';
 }
 
+var t1 = true;
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-    document.getElementById("header").style.marginLeft = "300px";
-    document.getElementById("datatable").style.marginLeft = "300px";
-   document.getElementById("logo").style.paddingRight= "300px";
+
+    if (t1) {
+        document.getElementById("mySidenav").style.width = "300px";
+        document.getElementById("header").style.marginLeft = "300px";
+        document.getElementById("datatable").style.marginLeft = "300px";
+        document.getElementById("logo").style.paddingRight = "300px";
+        t1 = false;
+    }
+    else
+    {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("header").style.marginLeft = "0";
+        document.getElementById("datatable").style.marginLeft = "0";
+        document.getElementById("logo").style.paddingRight= "0";
+        t1 = true;
+    }
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
